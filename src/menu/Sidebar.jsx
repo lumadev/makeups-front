1289e8eedccfa570ce79683bb1e622b18f609763
Luma Macagnan <luma.macagnan@gsurfnet.net>
2025-06-18 14:09:34 -0300

    Adicionada imagem de música em sidebar

--- src/menu/Sidebar.jsx
+++ src/menu/Sidebar.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom"
 
 import ItemMenu from './ItemMenu'
 
+import musicImg from '../assets/musica.jpg';
+
 const svgs = {
   class: 'M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z',
   students: 'M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z'
@@ -18,49 +20,25 @@ function Sidebar() {
         className="bg-white h-screen md:block shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300 ease-in-out"
         x-show="sidenav"
       >
-        <div className="space-y-6 md:space-y-10 mt-10">
+        <div className="md:space-y-10 mt-10">
           <h1 className="hidden md:block font-bold text-sm md:text-xl text-center">
             Reposições
           </h1>
-          <div id="profile" className="space-y-3">
-            <img
-              src="https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-              alt="Avatar user"
-              className="w-10 md:w-16 rounded-full mx-auto"
-            />
+          <div id="profile">
             <div>
+              <img
+                src={musicImg}
+                alt="Music"
+                className="rounded-full mx-auto"
+              />
+
               <h2
-                className="font-medium text-xs md:text-sm text-center text-teal-500"
+                className="mt-4 font-medium text-xs md:text-sm text-center text-teal-500"
               >
                 Weslley Joanes
               </h2>
               <p className="text-xs text-gray-500 text-center">Administrador</p>
             </div>
-          </div>
-          <div
-            className="flex border-2 border-gray-200 rounded-md focus-within:ring-2 ring-teal-500"
-          >
-            <input
-              type="text"
-              className="w-full rounded-tl-md rounded-bl-md px-2 py-3 text-sm text-gray-600 focus:outline-none"
-              placeholder="Search"
-            />
-            <button
-              className="rounded-tr-md rounded-br-md px-2 py-3 hidden md:block"
-            >
-              <svg
-                className="w-4 h-4 fill-current"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
           </div>
 
           {/* Sidebar menu */}

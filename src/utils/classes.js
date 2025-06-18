905457f293e4c0740e47f943d61c6b3a9a912e0e
Luma Macagnan <luma.macagnan@gsurfnet.net>
2025-06-18 15:47:49 -0300

    Criadas classes utilitárias de input e btn

---
+++ src/utils/classes.js
@@ -0,0 +1,15 @@
+const inputClass =
+  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg " +
+  "focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " +
+  "dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 " +
+  "dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+
+const inputLabelClass = 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+
+const btnClass = 'px-3 py-2 lg:px-4 bg-blue-500 collapse:bg-green-100 text-white text-sm font-semibold rounded hover:bg-blue-600'
+
+export { 
+  btnClass,
+  inputClass, 
+  inputLabelClass
+}

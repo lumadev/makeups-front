c9a64ee700ccf6b5ea654682dd1aff8f5db37e13
Luma Macagnan <luma.macagnan@gsurfnet.net>
2025-06-17 22:59:45 -0300

    Adicionadas rotas e componente NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css'
-import Sidebar from './menu/Sidebar'
+
+import Layout from './Layout'
+import NotFound from './errors/NotFound'
+import StudentList from './students/StudentList'
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Sidebar />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="alunos" element={<StudentList />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/errors/NotFound.jsx
@@ -0,0 +1,10 @@
+
+function NotFound() {
+  return (
+    <>
+      404 - Página não encontrada
+    </>
+  )
+}
+
+export default NotFound

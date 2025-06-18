68bcadfaf1552b5b17b7dc6a98cdbbd8edfac113
Luma Macagnan <luma.macagnan@gsurfnet.net>
2025-06-18 14:10:31 -0300

    Adicionada biblioteca de toast

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
 
 import Sidebar from './menu/Sidebar'
 
@@ -7,9 +8,11 @@ function Layout() {
     <div style={{ display: "flex", height: "100vh" }}>
       <Sidebar />
 
-      <main style={{ flex: 1, padding: "20px" }}>
+      <div style={{ flex: 1, padding: "20px" }}>
         <Outlet />
-      </main>
+
+        <ToastContainer />
+      </div>
     </div>
   )
 }

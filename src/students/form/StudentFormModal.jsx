714270887762ef6ef7522b26696202f71692d704
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-13 12:26:08 -0300

    Add token on student form (create and edit)

--- src/students/form/StudentFormModal.jsx
+++ src/students/form/StudentFormModal.jsx
@@ -2,8 +2,7 @@
 import { btnClass, btnCancelClass } from '../../utils/classes'
 import { toast } from 'react-toastify'
 import { useState, useEffect } from 'react'
-
-import axios from 'axios'
+import { saveStudent, editStudent } from "../../services/studentService";
 
 import LoadingButton from '../../components/LoadingButton'
 import Modal from "../../components/Modal";
@@ -19,11 +18,20 @@ export default function StudentFormModal({
   const [loadingSave, setLoadingSave] = useState(false)
   const [formData, setFormData] = useState({})
 
-  const addStudent = async () => {
-    setLoadingSave(true)
+  const saveOrEdit = () => {
+    if (isEdit) {
+      const idStudent = formData.id
+      return editStudent(idStudent, formData);
+    } else {
+      return saveStudent(formData);
+    }
+  }
 
+  const save = async () => {
+    setLoadingSave(true)
+    
     try {
-      await axios.post('http://localhost:3000/students', formData)
+      await saveOrEdit()
 
       toast("Aluno salvo com sucesso", { 
         type: 'success'
@@ -36,39 +44,8 @@ export default function StudentFormModal({
         type: 'error'
       })
     } finally {
-      onClose()
-      setLoadingSave(false)
-    }
-  }
-  
-  const editStudent = async () => {
-    setLoadingSave(true)
-
-    const idStudent = formData.id
-    try {
-      await axios.put(`http://localhost:3000/students/${idStudent}`, formData)
-
-      toast("Aluno salvo com sucesso", { 
-        type: 'success'
-      })
-
-      // update list with the student updated
-      onStudentSaved()
-    } catch {
-      toast("Ocorreu um erro ao editar o aluno", { 
-        type: 'error'
-      })
-    } finally {
       setLoadingSave(false)
       onClose()
-    }
-  }
-
-  const save = () => {
-    if (isEdit) {
-      editStudent()
-    } else {
-      addStudent()
     }
   }
 

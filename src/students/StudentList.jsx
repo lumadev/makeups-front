abebcd1986d7a36dbbb2c1d1083f3ee80e543b16
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-27 18:51:06 -0300

    fix: show update message after save students data

--- src/students/StudentList.jsx
+++ src/students/StudentList.jsx
@@ -57,73 +57,83 @@ function StudentList({ searchTerm, reloadFlag, onCountChange }) {
         <div>
           <span className="ml-2">Carregando lista...</span>
         </div>
-      ) : students.length > 0 ? (
-        <section className="container">
-          <div>
-            <div className="min-w-full py-2 align-middle">
-              <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                  <thead className="bg-gray-50 dark:bg-gray-800">
-                    <tr>
-                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        Nome
-                      </th>
-
-                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        Telefone
-                      </th>
-
-                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        Data do Cadastro
-                      </th>
-
-                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        Ações
-                      </th>
-                    </tr>
-                  </thead>
-
-                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                    {filteredStudents.map((student, index) => (
-                      <tr key={index}>
-                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                          <div className="flex items-center gap-x-2">
-                            <div>
-                              <h2 className="text-sm font-medium text-gray-800 dark:text-white ">
-                                {student.name}
-                              </h2>
-                              <p className="text-xs font-normal text-gray-600 dark:text-gray-400">
-                                {student.email}
-                              </p>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                          {applyMaskPhone('(99) 99999-9999', student.phone)}
-                        </td>
-                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                          {formatDate(student.dateRegister)}
-                        </td>
-                        <td className="px-4 py-4 text-sm whitespace-nowrap">
-                          <div className="flex items-center gap-x-6">
-                            <StudentActions
-                              student={student}
-                              onAfterSave={onAfterSave}
-                            />
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </section>
       ) : (
-        <div>
-          <p className="text-gray-400 font-medium">Nenhum aluno encontrado :(</p>
-        </div>
+        <>
+          {loadingAfterSave && (
+            <div>
+              <span>Atualizando lista...</span>
+            </div>
+          )}
+
+          {students.length > 0 ? (
+            <section className="container">
+              <div>
+                <div className="min-w-full py-2 align-middle">
+                  <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                      <thead className="bg-gray-50 dark:bg-gray-800">
+                        <tr>
+                          <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            Nome
+                          </th>
+
+                          <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            Telefone
+                          </th>
+
+                          <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            Data do Cadastro
+                          </th>
+
+                          <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            Ações
+                          </th>
+                        </tr>
+                      </thead>
+
+                      <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                        {filteredStudents.map((student, index) => (
+                          <tr key={index}>
+                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                              <div className="flex items-center gap-x-2">
+                                <div>
+                                  <h2 className="text-sm font-medium text-gray-800 dark:text-white ">
+                                    {student.name}
+                                  </h2>
+                                  <p className="text-xs font-normal text-gray-600 dark:text-gray-400">
+                                    {student.email}
+                                  </p>
+                                </div>
+                              </div>
+                            </td>
+                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                              {applyMaskPhone('(99) 99999-9999', student.phone)}
+                            </td>
+                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                              {formatDate(student.dateRegister)}
+                            </td>
+                            <td className="px-4 py-4 text-sm whitespace-nowrap">
+                              <div className="flex items-center gap-x-6">
+                                <StudentActions
+                                  student={student}
+                                  onAfterSave={onAfterSave}
+                                />
+                              </div>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </section>
+          ) : (
+            <div className="mt-2">
+              <p className="text-gray-400 font-medium">Nenhum aluno encontrado :(</p>
+            </div>
+          )}
+        </>
       )}
     </div>
   )

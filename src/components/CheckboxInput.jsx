2bec180bc2290d1f2d64d987b7f9bdb5082afd19
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-27 16:38:20 -0300

    feature: add checkbox global component

---
+++ src/components/CheckboxInput.jsx
@@ -0,0 +1,16 @@
+function CheckboxInput({ label, checked, onChange, name }) {
+  return (
+    <label className="flex items-center space-x-2 cursor-pointer select-none">
+      <input
+        type="checkbox"
+        name={name}
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+        className="h-5 w-5 rounded border-gray-300 accent-blue-600 transition duration-200 focus:outline-none"
+      />
+      <span className="text-gray-700">{label}</span>
+    </label>
+  );
+}
+
+export default CheckboxInput

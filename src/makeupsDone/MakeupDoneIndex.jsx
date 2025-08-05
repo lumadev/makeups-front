da6b68c09d00daa3981a15c034d85e9353470826
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-04 23:22:01 -0300

    create MakeupDoneIndex logic

---
+++ src/makeupsDone/MakeupDoneIndex.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import MakeupList from '../makeups/list/MakeupList'
+import MakeupSearch  from '../makeups/list/MakeupSearch'
+
+function MakeupDoneIndex() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [makeupCount, setMakeupCount] = useState(0)
+
+  return (
+    <>
+      {/* Makeup search */}
+      {makeupCount > 0 && (
+        <MakeupSearch searchTerm={searchTerm} onSearch={setSearchTerm} />
+      )}
+
+      {/* Makeups list */}
+      <MakeupList 
+        title="Reposições"
+        screenType="makeups-done"
+        searchTerm={searchTerm} 
+        onCountChange={setMakeupCount}
+      />
+    </>
+  )
+}
+
+export default MakeupDoneIndex

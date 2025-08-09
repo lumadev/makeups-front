4baaa913d958da30f8899779fd98011678bc0159
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-09 17:14:34 -0300

    improvement on MakeupInfo colors

--- src/makeups/MakeupInfo.jsx
+++ src/makeups/MakeupInfo.jsx
@@ -3,13 +3,11 @@ import { formatDate } from '../utils/date'
 
 function MakeupInfo({ makeups = [] }) {
   const todaysCount = useMemo(() => {
-    
     const makeupsToday = makeups.filter((makeup) => {
       const dateReplacement = makeup.dateReplacement
-      
-      // in caso of the date to be empty
+
       if (!dateReplacement) return false
-      
+
       const todayFormatted = formatDate(new Date())
       const dateFormatted = formatDate(dateReplacement)
 
@@ -18,20 +16,15 @@ function MakeupInfo({ makeups = [] }) {
     return makeupsToday.length
   }, [makeups])
 
-  // Renderiza o aviso **apenas se** houver reposições para hoje
-  if (todaysCount > 0) {
-    return (
-      <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4 rounded">
-        Você tem {todaysCount} reposição(ões) para hoje!
-      </div>
-    )
-  } else {
-    return (
-      <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4 rounded">
-        Você não tem reposições para hoje!
-      </div>
-    )
-  }
+  const baseClasses = "bg-blue-50 text-blue-800 p-4 mb-4 rounded-lg shadow-sm"
+
+  return (
+    <div className={baseClasses}>
+      {todaysCount > 0
+        ? `Você tem ${todaysCount} reposição(ões) para hoje!`
+        : "Você não tem reposições para hoje!"}
+    </div>
+  )
 }
 
 export default MakeupInfo

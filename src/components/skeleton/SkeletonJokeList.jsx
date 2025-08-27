8549793f4ad4c2e7148c14e14f3ee33f7e22f88a
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-26 21:31:03 -0300

    set skeleton pattern in SkeletonJokeList

--- src/components/skeleton/SkeletonJokeList.jsx
+++ src/components/skeleton/SkeletonJokeList.jsx
@@ -1,39 +1,35 @@
+import TableHeaderCell from '../table/TableHeaderCell'
+import TableDataCell from '../table/TableDataCell'
+
 function SkeletonJokeList() {
   return (
-    <div className="mt-4">
+    <section className="container mt-2">
+      <div className="flex items-center justify-between mb-2">
+        <div className="h-6 bg-gray-200 dark:bg-gray-700 rounded w-48 animate-pulse" />
+      </div>
       <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
         <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
           <thead className="bg-gray-50 dark:bg-gray-800">
             <tr>
-              <th
-                scope="col"
-                className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-300"
-              >
-                Descrição
-              </th>
-              <th
-                scope="col"
-                className="px-4 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-300"
-              >
-                Tipo
-              </th>
+              <TableHeaderCell>Descrição</TableHeaderCell>
+              <TableHeaderCell>Tipo</TableHeaderCell>
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-            {[...Array(5)].map((_, index) => (
-              <tr key={index}>
-                <td className="px-4 py-4">
-                  <div className="h-4 w-3/4 animate-pulse rounded bg-gray-200 dark:bg-gray-700"></div>
-                </td>
-                <td className="px-4 py-4">
-                  <div className="h-4 w-1/4 animate-pulse rounded bg-gray-200 dark:bg-gray-700"></div>
-                </td>
+            {[...Array(5)].map((_, idx) => (
+              <tr key={idx} className="animate-pulse">
+                <TableDataCell>
+                  <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-3/4" />
+                </TableDataCell>
+                <TableDataCell>
+                  <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-1/4" />
+                </TableDataCell>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-    </div>
+    </section>
   )
 }
 

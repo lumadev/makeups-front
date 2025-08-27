3174ebc2bb4b4eb7f8f291ca3b3f70d497b00245
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-26 21:33:45 -0300

    update SkeletonStudentList to be updated with list column changes

--- src/components/skeleton/SkeletonStudentList.jsx
+++ src/components/skeleton/SkeletonStudentList.jsx
@@ -12,33 +12,24 @@ function SkeletonStudentList() {
           <thead className="bg-gray-50 dark:bg-gray-800">
             <tr>
               <TableHeaderCell>Nome</TableHeaderCell>
-              <TableHeaderCell>Telefone</TableHeaderCell>
-              <TableHeaderCell>Data do Cadastro</TableHeaderCell>
+              <TableHeaderCell>Data do cadastro</TableHeaderCell>
               <TableHeaderCell>Ações</TableHeaderCell>
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
             {[...Array(5)].map((_, idx) => (
               <tr key={idx} className="animate-pulse">
-                {/* Nome + email */}
+                {/* Name */}
                 <TableDataCell>
-                  <div className="flex flex-col gap-1">
-                    <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-32" />
-                    <div className="h-3 bg-gray-200 dark:bg-gray-700 rounded w-40" />
-                  </div>
+                  <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-32" />
                 </TableDataCell>
 
-                {/* Telefone */}
-                <TableDataCell>
-                  <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-28" />
-                </TableDataCell>
-
-                {/* Data */}
+                {/* Register Date */}
                 <TableDataCell>
                   <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-24" />
                 </TableDataCell>
 
-                {/* Ações */}
+                {/* Actions */}
                 <TableDataCell>
                   <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-16" />
                 </TableDataCell>

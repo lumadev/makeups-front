9a05d818206ff44822c77af1f1ed9ff4153e6e27
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-31 20:51:23 -0300

    show skeleton loader when student is loading

--- src/features/students/StudentManage.jsx
+++ src/features/students/StudentManage.jsx
@@ -29,7 +29,16 @@ function StudentManage() {
     fetchStudent()
   }, [id])
 
-  if (loading) return <p>Carregando...</p>
+  if (loading) {
+    return (
+      <div className="p-4 animate-pulse">
+        <div className="h-7 bg-gray-200 dark:bg-gray-700 rounded w-72 mb-4" />
+        <div className="h-5 bg-gray-200 dark:bg-gray-700 rounded w-full mb-2" />
+        <div className="h-5 bg-gray-200 dark:bg-gray-700 rounded w-3/4 mb-2" />
+        <div className="h-5 bg-gray-200 dark:bg-gray-700 rounded w-2/3" />
+      </div>
+    )
+  }
 
   return (
     <div className="p-4">

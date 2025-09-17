b1c62b7e5c78aa02ea2d68ef4387e5341bedaabb
Luma Macagnan <lumamacagnan@gmail.com>
2025-09-17 00:44:22 -0300

    feature: create new sidebar menu item

--- src/features/menu/Sidebar.jsx
+++ src/features/menu/Sidebar.jsx
@@ -13,6 +13,7 @@ import {
 import ItemMenu from './ItemMenu'
 import musicImg from '../../assets/musica.jpg'
 import { IconJoker } from '@tabler/icons-react'
+import { IconMusic } from '@tabler/icons-react'
 
 function Sidebar() {
   const location = useLocation()
@@ -55,6 +56,7 @@ function Sidebar() {
     { title: "Reposições", path: "/reposicoes", icon: <IconSchool size={20} />, evenodd: "true" },
     { title: "Concluídas", path: "/reposicoes-concluidas", icon: <IconCheck size={20} />, evenodd: "true" },
     { title: "Alunos", path: "/alunos", icon: <IconUser size={20} /> },
+    { title: "Músicas de Alunos", path: "/musicas-alunos", icon: <IconMusic size={20} /> },
     { title: "Datas de Evento", path: "/datas-de-evento", icon: <IconCalendarEvent size={20} /> },
     { title: "Piadas", path: "/piadas", icon: <IconJoker size={20} /> },
   ]

e0c332e1314acc6cba3574e9aaca2a821e5bd647
Luma Macagnan <lumamacagnan@gmail.com>
2025-09-17 00:43:28 -0300

    refactor in student songs service and create list all method

--- src/features/students/songs/studentSongsService.js
+++ src/features/students/songs/studentSongsService.js
@@ -1,32 +1,39 @@
 import { api } from "@/services/api"
 
-const basePath = (studentId) => `/student-songs/${studentId}/songs`
+
+const basePath = '/student-songs'
+const basePathStudent = (studentId) => `${basePath}/${studentId}/songs`
+
+const listAllStudentSongs = () => {
+  return api.get(basePath)
+}
 
 const listStudentSongs = (studentId) => {
-  return api.get(basePath(studentId))
+  return api.get(basePathStudent(studentId))
 }
 
 const listDoneStudentSongs = (studentId) => {
-  return api.get(`${basePath(studentId)}-done`)
+  return api.get(`${basePathStudent(studentId)}-done`)
 }
 
 const listNotDoneStudentSongs = (studentId) => {
-  return api.get(`${basePath(studentId)}-not-done`)
+  return api.get(`${basePathStudent(studentId)}-not-done`)
 }
 
 const saveStudentSong = (studentId, data) => {
-  return api.post(basePath(studentId), data)
+  return api.post(basePathStudent(studentId), data)
 }
 
 const editStudentSong = (studentId, songId, data) => {
-  return api.put(`${basePath(studentId)}/${songId}`, data)
+  return api.put(`${basePathStudent(studentId)}/${songId}`, data)
 }
 
 const deleteStudentSong = (studentId, songId) => {
-  return api.delete(`${basePath(studentId)}/${songId}`)
+  return api.delete(`${basePathStudent(studentId)}/${songId}`)
 }
 
 export { 
+  listAllStudentSongs,
   listStudentSongs,
   listDoneStudentSongs,
   listNotDoneStudentSongs,

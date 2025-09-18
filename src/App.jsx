331b153f214e347ccb6c8f3d2a6afb077770a47b
Luma Macagnan <lumamacagnan@gmail.com>
2025-09-18 19:15:06 -0300

    refactor: reorganize routes file for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 
-import './App.css'
+import "./App.css"
 
-import Layout from './Layout'
-import Login from './features/auth/Login'
-import EventDateIndex from './features/eventDates/EventDateIndex'
-import HomeIndex from './features/home/HomeIndex'
-import MakeupIndex from './features/makeups/MakeupIndex'
-import MakeupDoneIndex from './features/makeupsDone/MakeupDoneIndex'
-import NotFound from './errors/NotFound'
-import RequireAuth from './RequireAuth'
-import StudentIndex from './features/students/StudentIndex'
-import StudentManage from './features/students/StudentManage'
-import StudentAllSongsIndex from './features/studentSongs/StudentAllSongsIndex'
-import JokeIndex from './features/jokes/JokeIndex'
+import Login from "./features/auth/Login"
+import Layout from "./Layout"
+import LayoutHome from "./LayoutHome"
+import EventDateIndex from "./features/eventDates/EventDateIndex"
+import HomeIndex from "./features/home/HomeIndex"
+import MakeupIndex from "./features/makeups/MakeupIndex"
+import MakeupDoneIndex from "./features/makeupsDone/MakeupDoneIndex"
+import NotFound from "./errors/NotFound"
+import RequireAuth from "./RequireAuth"
+import StudentIndex from "./features/students/StudentIndex"
+import StudentManage from "./features/students/StudentManage"
+import StudentAllSongsIndex from "./features/studentSongs/StudentAllSongsIndex"
+import JokeIndex from "./features/jokes/JokeIndex"
 
 function App() {
+  const protectedRoutes = [
+    { path: "alunos", element: <StudentIndex /> },
+    { path: "aluno/:id/musicas", element: <StudentManage /> },
+    { path: "musicas-alunos", element: <StudentAllSongsIndex /> },
+    { path: "reposicoes", element: <MakeupIndex /> },
+    { path: "reposicoes-concluidas", element: <MakeupDoneIndex /> },
+    { path: "datas-de-evento", element: <EventDateIndex /> },
+    { path: "piadas", element: <JokeIndex /> },
+  ]
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />}>
-        </Route>
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
 
         {/* Protected routes */}
         <Route element={<RequireAuth />}>
+          {/* Home separado se precisar de outro layout */}
+          <Route path="/" element={<LayoutHome />}>
+            <Route path="home" element={<HomeIndex />} />
+          </Route>
+
+          {/* App principal */}
           <Route path="/" element={<Layout />}>
             <Route index element={<Navigate to="/reposicoes" replace />} />
-            <Route path="alunos" element={<StudentIndex />} />
-            <Route path="aluno/:id/musicas" element={<StudentManage />} />
-            <Route path="home" element={<HomeIndex />} />
-            <Route path="musicas-alunos" element={<StudentAllSongsIndex />} />
-            <Route path="reposicoes" element={<MakeupIndex />} />
-            <Route path="reposicoes-concluidas" element={<MakeupDoneIndex />} />
-            <Route path="datas-de-evento" element={<EventDateIndex />} />
-            <Route path="piadas" element={<JokeIndex />} />
+
+            {protectedRoutes.map(({ path, element }) => (
+              <Route key={path} path={path} element={element} />
+            ))}
+
             <Route path="*" element={<NotFound />} />
           </Route>
         </Route>

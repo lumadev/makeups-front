841f825a0f68bb9492239cbd1fd7fa7dd257a284
Luma Macagnan <lumamacagnan@gmail.com>
2025-09-22 01:47:50 -0300

    create getTrackDetails

--- src/services/spotifyService.js
+++ src/services/spotifyService.js
@@ -6,6 +6,11 @@ const listSongsInfo = (songName, artist) => {
   return api.get(`${basePath}/search-song?songName=${songName}&artist=${artist}`)
 }
 
+const getTrackDetails = (spotifyId) => {
+  return api.get(`${basePath}/track-details?spotifyId=${spotifyId}`)
+}
+
 export { 
-  listSongsInfo
+  listSongsInfo,
+  getTrackDetails
 }

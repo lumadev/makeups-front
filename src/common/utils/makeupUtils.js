b8697bf93c0bf2ee30d00e14e05b79abbca922b6
Luma Macagnan <lumamacagnan@gmail.com>
2026-02-15 16:29:05 -0300

    feature: add date validation to makeup form

--- src/common/utils/makeupUtils.js
+++ src/common/utils/makeupUtils.js
@@ -1,3 +1,14 @@
+/**
+ * Verifica se a data de reposição é igual ou posterior à data original
+ */
+function isValidFutureDate(oldDate, newDate) {
+  const dateOld = new Date(oldDate)
+  const dateReplacement = new Date(newDate)
+
+  // Compara os timestamps das datas
+  return dateReplacement >= dateOld
+}
+
 function validateMakeupForm(formData) {
   const error = {
     isValid: false,
@@ -14,10 +25,18 @@ function validateMakeupForm(formData) {
     return error
   }
 
-  if (!formData.isOpenDate && !formData.dateReplacement) {
-    error.errorMessage = 'Nova data de reposição não preenchida.'
-    return error
+  if (!formData.isOpenDate) {
+    if (!formData.dateReplacement) {
+      error.errorMessage = 'Nova data de reposição não preenchida.'
+      return error
+    }
+
+    if (!isValidFutureDate(formData.dateOld, formData.dateReplacement)) {
+      error.errorMessage = 'A nova data não pode ser anterior à data original.'
+      return error
+    }
   }
+
   return false
 }
 

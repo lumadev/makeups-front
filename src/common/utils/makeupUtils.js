65478e9079f1300e5c8c17e3dd70c36a5f31fdfb
Luma Macagnan <lumamacagnan@gmail.com>
2026-02-25 00:07:48 -0300

    validation removal of valid future date

--- src/common/utils/makeupUtils.js
+++ src/common/utils/makeupUtils.js
@@ -1,14 +1,3 @@
-/**
- * Verifica se a data de reposição é igual ou posterior à data original
- */
-function isValidFutureDate(oldDate, newDate) {
-  const dateOld = new Date(oldDate)
-  const dateReplacement = new Date(newDate)
-
-  // Compara os timestamps das datas
-  return dateReplacement >= dateOld
-}
-
 function validateMakeupForm(formData) {
   const error = {
     isValid: false,
@@ -28,11 +17,6 @@ function validateMakeupForm(formData) {
   if (!formData.isOpenDate) {
     if (!formData.dateReplacement) {
       error.errorMessage = 'Nova data de reposição não preenchida.'
-      return error
-    }
-
-    if (!isValidFutureDate(formData.dateOld, formData.dateReplacement)) {
-      error.errorMessage = 'A nova data não pode ser anterior à data original.'
       return error
     }
   }
